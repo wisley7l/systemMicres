7a2964a104cb75e66720bfa1d0690db32ba77f83
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-12 09:49:07 -0300

    updadte 12-06

--- buscaveiculo.php
+++ buscaveiculo.php
@@ -28,18 +28,18 @@
 
 	# Executa a query desejada
 	// $row = $dbh->query("SELECT cnpj,nome,nveiculo FROM empresa WHERE cnpj = $id LIMIT 1")->fetch();
-	$row = $dbh->query("SELECT 'v.placa','v.cnpj_empresa','v.marca',v'.modelo','v.pneususo','v.pneus','e.nome' FROM veiculo v, empresa e WHERE placa = '$id' AND v.cnpj_empresa = e.cnpj LIMIT 1")->fetch();
-
+	// $row = $dbh->query("SELECT 'v.placa','v.cnpj_empresa','v.marca',v'.modelo','v.pneususo','v.pneus','e.nome' FROM veiculo v, empresa e WHERE placa = '$id' AND v.cnpj_empresa = e.cnpj LIMIT 1")->fetch();
+$row = $dbh->query("SELECT * FROM veiculo v, empresa e WHERE placa = '$id' AND v.cnpj_empresa = e.cnpj LIMIT 1")->fetch();
 
 // /*
 	// $row = $stmt->fetch()
-	$user = array('placa' => $row['v.placa'],
-		 'cnpj_empresa' => $row['v.cnpj_empresa'],
-		 'marca' => $row['v.marca'],
-		 'modelo' => $row['v.modelo'],
-		 'pneususo' => $row['v.pneususo'],
-		 'pneus' => $row['v.pneus'],
-	 );
+	// $user = array('placa' => $row['v.placa'],
+	// 	 'cnpj_empresa' => $row['v.cnpj_empresa'],
+	// 	 'marca' => $row['v.marca'],
+	// 	 'modelo' => $row['v.modelo'],
+	// 	 'pneususo' => $row['v.pneususo'],
+	// 	 'pneus' => $row['v.pneus'],
+	//  );
 	 # Executa a query desejada
  	// $stmt = $dbh->query("SELECT cod,veiculo,status,pos_veicu FROM pneu WHERE veiculo = $id");
 	//
