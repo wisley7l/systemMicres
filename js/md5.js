29206501274a0ff2df37006ea993bf511059ae1d
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-15 12:51:56 -0300

    update 15-06

--- js/md5.js
+++ js/md5.js
@@ -1,16 +1,27 @@
 console.log("MD5 wisley");
 
-//+ Jonas Raoni Soares Silva
-//# http://jsfromhell.com/string/rot13 [rev. #1]
-String.prototype.rot13 = function(){
-    return this.replace(/[a-zA-Z]/g, function(c){
-    return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
-    });
-};
-// var s = "My String";
-// var enc = s.rot13(); // encrypted value in enc
+
+function encode1(str) {
+  var encoded = "";
+  for (i=0; i<str.length;i++) {
+  var a = str.charCodeAt(i);
+  var b = a ^ 51; // bitwise XOR with any number, e.g. 123
+  encoded = encoded+String.fromCharCode(b);
+  }
+  return encoded;
+}
+
+
+
+base32 = new Nibbler({
+    dataBits: 8,
+    codeBits: 5,
+    keyString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',
+    pad: '='
+});
 
 
 var MD5 = function (string) {
-  return string.rot13();
+
+  return encode1(string);
 }
