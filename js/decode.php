3d513ad5990b28312433e7b6eaecb4f767ed3f7a
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-15 12:43:40 -0300

    update 15-06

--- js/decode.php
+++ js/decode.php
@@ -1,15 +1,15 @@
 <?php
 
-function decode(){
-  $encoded = "..."; // <-- encoded string from the request
-  $decoded = "";
-  for( $i = 0; $i < strlen($encoded); $i++ ) {
-  $b = ord($encoded[$i]);
-  $a = $b ^ 123; // <-- must be same number used to encode the character
-  $decoded .= chr($a)
-  }
-  echo $decoded;  
-}
+// function decode(){
+//   $encoded = "..."; // <-- encoded string from the request
+//   $decoded = "";
+//   for( $i = 0; $i < strlen($encoded); $i++ ) {
+//   $b = ord($encoded[$i]);
+//   $a = $b ^ 123; // <-- must be same number used to encode the character
+//   $decoded .= chr($a)
+//   }
+//   echo $decoded;
+// }
 
 
 ?>
