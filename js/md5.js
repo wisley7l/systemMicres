de69f515416b1890bdc354114948b047abe076a3
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-15 11:50:08 -0300

    update 15-06

--- js/md5.js
+++ js/md5.js
@@ -1,15 +1,5 @@
 console.log("MD5 wisley");
 
-var MD5 = function (string) {
-  var carcteres = string.split('');
-  for (var i = 0; i < carcteres.length; i++) {
-    carcteres[i] = carcteres[i].charCodeAt(0);
-  }
-  // console.log(carcteres.join("x"));
-  return carcteres.join("x");
-}
-
-
 base32 = new Nibbler({
     dataBits: 8,
     codeBits: 5,
@@ -17,7 +7,8 @@ base32 = new Nibbler({
     pad: '='
 });
 
-console.log(base32.encode("Hello, World") );
-console.log(base32.decode("IFWG62DB") );
-// base32.encode("Hello, World");  // "JBSWY3DPFQQFO33SNRSCC====="
-// base32.decode("IFWG62DB");      // "Aloha"
+
+var MD5 = function (string) {
+
+  return base32.encode(string);
+}
