bcdf4a37fadad3c22931b25bba0be307ea931551
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-15 12:12:40 -0300

    update 15-06

--- js/decode.php
+++ js/decode.php
@@ -1,12 +1,12 @@
 <?php
-namespace Base32;
-
-    public static function decode($encoded){
+    function decode($encoded){
       $decoded = "";
       for( $i = 0; $i < strlen($encoded); $i++ ) {
         $b = ord($encoded[$i]);
         $a = $b ^ 123;  // <-- must be same number used to encode the character
         $decoded .= chr($a)
       }
-      return $decoded
+      return $decoded;
     }
+
+?>
