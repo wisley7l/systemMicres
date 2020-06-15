5eac54fa209be786a0c22a31ed7770314b0eca33
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-15 13:04:05 -0300

    update 15-06

--- cadastrafunc.php
+++ cadastrafunc.php
@@ -43,8 +43,8 @@
 			// $decoded = decode($_GET['info']);
 			// echo $decoded;
 			$b = new Base32;
-			$outstr = $b->toString($_GET['info']));
-			echo $outstr;
+			// $outstr = $b->toString($_GET['info']));
+			// echo $outstr;
 		}
 	}
 
