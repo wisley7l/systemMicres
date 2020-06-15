d04fbc89b1db18ea68b0bc4972245fd73dde5644
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-15 11:33:52 -0300

    update 15-06

--- js/md5.js
+++ js/md5.js
@@ -3,7 +3,7 @@ console.log("MD5 wisley");
 var MD5 = function (string) {
   var carcteres = string.split('');
   for (var i = 0; i < carcteres.length; i++) {
-    carcteres[i] = parseInt(carcteres[i]);
+    carcteres[i] = carcteres[i].charCodeAt(0);
 
   }
   console.log(carcteres);
