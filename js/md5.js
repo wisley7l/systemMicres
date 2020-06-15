8f628cc8c4d9fbde04315acf17ed9c84dbe1bd57
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-15 12:52:31 -0300

    update 15-06

--- js/md5.js
+++ js/md5.js
@@ -1,18 +1,6 @@
 console.log("MD5 wisley");
 
 
-function encode1(str) {
-  var encoded = "";
-  for (i=0; i<str.length;i++) {
-  var a = str.charCodeAt(i);
-  var b = a ^ 51; // bitwise XOR with any number, e.g. 123
-  encoded = encoded+String.fromCharCode(b);
-  }
-  return encoded;
-}
-
-
-
 base32 = new Nibbler({
     dataBits: 8,
     codeBits: 5,
@@ -23,5 +11,5 @@ base32 = new Nibbler({
 
 var MD5 = function (string) {
 
-  return encode1(string);
+  return base32.encode(string);
 }
