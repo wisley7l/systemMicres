ba50819f65e1d223e53555a84ad3d92103a4446e
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-16 14:54:57 -0300

    upadadte 16-6

--- cadastraveiculo.php
+++ cadastraveiculo.php
@@ -93,7 +93,18 @@
 				 'pneususo' => $row3['pneususo'],
 				 'pneus' => $row2['pneus'],
 			);
-		}else {
+		}elseif ($_GET['info'] == "c") {
+			$row1 = $dbh->query("SELECT placa,cnpj_empresa,marca,modelo,pneususo,pneus FROM veiculo WHERE placa = '$id' LIMIT 1")->fetch();
+			$row2 = $dbh->query("SELECT COUNT(cod) as pneus FROM pneu WHERE veiculo ='$id'")->fetch();
+			$row3 = $dbh->query("SELECT COUNT(cod) as pneususo FROM pneu WHERE veiculo ='$id' AND status = 1")->fetch();
+			$user = array('placa' => $row1['placa'],
+				 'cnpj_empresa' => $row1['cnpj_empresa'],
+				 'marca' => $row1['marca'],
+				 'modelo' => $row1['modelo'],
+				 'pneususo' => $row3['pneususo'],
+				 'pneus' => $row2['pneus'],
+			);
+		}else{
 			$row = $dbh->query("SELECT placa,cnpj_empresa,marca,modelo FROM veiculo WHERE cnpj_empresa = $cnpj_empresa LIMIT 1")->fetch();
 			$user = array('placa' => $placa,
 				 'cnpj_empresa' => $row['cnpj_empresa'],
