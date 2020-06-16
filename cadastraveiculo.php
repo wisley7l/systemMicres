e33a5bc96e3e00a14ee9e9af67092a76c02adc22
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-16 14:47:14 -0300

    upadadte 16-6

--- cadastraveiculo.php
+++ cadastraveiculo.php
@@ -63,7 +63,8 @@
 				$row = $dbh->query("INSERT INTO veiculo (placa,cnpj_empresa,marca,modelo,pneususo,pneus) VALUES ('$placa',$cnpj_empresa,'$marca','$modelo',0,0)")->fetch();
 				$row5 = $dbh->query("SELECT count(placa) FROM veiculo WHERE cnpj_empresa = $cnpj_empresa")->fetch();
 				$dbh->closeCursor();
-				$row6 = $dbh->query("UPDATE empresa	SET nveiculo = $row5[0] WHERE cnpj = $cnpj_empresa")->fetch();
+				echo "<p>$row5[0]</p>";
+				// $row6 = $dbh->query("UPDATE empresa	SET nveiculo = $row5[0] WHERE cnpj = $cnpj_empresa")->fetch();
 				// header('Location: buscaempresa.php?cnpj=' . $cnpj_empresa);
 
 			}elseif ($_GET['info'] == "u") {
