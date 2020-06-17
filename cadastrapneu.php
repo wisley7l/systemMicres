5ba3ee144aeae3058959da7894da6db99182335e
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-17 10:58:35 -0300

    update 17-06

--- cadastrapneu.php
+++ cadastrapneu.php
@@ -21,8 +21,8 @@
 	$dbh = new PDO('mysql:host='.$hostname .';dbname='. $database, $user, $password);
 
 	// /*
-	if (isset($_GET['placa'])){
-		$id = $_GET['placa'];
+	if (isset($_GET['cod'])){
+		$id = $_GET['cod'];
 	}
 
 	if (isset($_GET['json'])){
@@ -45,32 +45,32 @@
 
 			$pieces = explode(",", $outstr);
 
-			$placa =  strtoupper($pieces[1]);
-			$cnpj_empresa = (int)$pieces[2];
-			$marca = $pieces[3];
-			$modelo = $pieces[4];
-			$pneususo = (int)$pieces[5];
-			$pneus = (int)$pieces[6];
+			$cod =  strtoupper($pieces[1]);
+			$veiculo= (int)$pieces[2];
+			$status = $pieces[3];
+			$pos= $pieces[4];
+			// $pneususo = (int)$pieces[5];
+			// $pneus = (int)$pieces[6];
 
 			if ($_GET['info'] == "c") {
 				echo "cadastro";
-				$row = $dbh->query("INSERT INTO veiculo (placa,cnpj_empresa,marca,modelo,pneususo,pneus) VALUES ('$placa',$cnpj_empresa,'$marca','$modelo',0,0)")->fetch();
-				$row5 = $dbh->query("SELECT count(placa) FROM veiculo WHERE cnpj_empresa = $cnpj_empresa")->fetch();
-				$row6 = $dbh->query("UPDATE empresa	SET nveiculo = $row5[0] WHERE cnpj = $cnpj_empresa")->fetch();
-				header('Location: buscaempresa.php?cnpj=' . $cnpj_empresa);
+				// $row = $dbh->query("INSERT INTO veiculo (placa,cnpj_empresa,marca,modelo,pneususo,pneus) VALUES ('$placa',$cnpj_empresa,'$marca','$modelo',0,0)")->fetch();
+				// $row5 = $dbh->query("SELECT count(placa) FROM veiculo WHERE cnpj_empresa = $cnpj_empresa")->fetch();
+				// $row6 = $dbh->query("UPDATE empresa	SET nveiculo = $row5[0] WHERE cnpj = $cnpj_empresa")->fetch();
+				// header('Location: buscaempresa.php?cnpj=' . $cnpj_empresa);
 
 			}elseif ($_GET['info'] == "u") {
 				echo "updadte";
-				$row = $dbh->query("UPDATE veiculo SET cnpj_empresa = $cnpj_empresa , marca = '$marca' ,modelo = '$modelo',pneususo= $pneususo,pneus=$pneus  WHERE placa ='$placa'")->fetch();
-				header('Location: buscaempresa.php?cnpj=' . $cnpj_empresa);
+				// $row = $dbh->query("UPDATE veiculo SET cnpj_empresa = $cnpj_empresa , marca = '$marca' ,modelo = '$modelo',pneususo= $pneususo,pneus=$pneus  WHERE placa ='$placa'")->fetch();
+				// header('Location: buscaempresa.php?cnpj=' . $cnpj_empresa);
 			}
 			elseif ($_GET['info'] == "d") {
 				echo "delete";
-				$row = $dbh->query("DELETE FROM veiculo WHERE placa ='$placa'")->fetch();
-				$row5 = $dbh->query("SELECT count(placa) FROM veiculo WHERE cnpj_empresa = $cnpj_empresa")->fetch();
-				// $dbh->closeCursor();
-				$row6 = $dbh->query("UPDATE empresa	SET nveiculo = $row5[0] WHERE cnpj = $cnpj_empresa")->fetch();
-				header('Location: buscaempresa.php?cnpj=' . $cnpj_empresa);
+				// $row = $dbh->query("DELETE FROM veiculo WHERE placa ='$placa'")->fetch();
+				// $row5 = $dbh->query("SELECT count(placa) FROM veiculo WHERE cnpj_empresa = $cnpj_empresa")->fetch();
+				// // $dbh->closeCursor();
+				// $row6 = $dbh->query("UPDATE empresa	SET nveiculo = $row5[0] WHERE cnpj = $cnpj_empresa")->fetch();
+				// header('Location: buscaempresa.php?cnpj=' . $cnpj_empresa);
 			}
 		}
 	}
@@ -81,32 +81,32 @@
 			$row1 = $dbh->query("SELECT placa,cnpj_empresa,marca,modelo,pneususo,pneus FROM veiculo WHERE placa = '$id' LIMIT 1")->fetch();
 			$row2 = $dbh->query("SELECT COUNT(cod) as pneus FROM pneu WHERE veiculo ='$id'")->fetch();
 			$row3 = $dbh->query("SELECT COUNT(cod) as pneususo FROM pneu WHERE veiculo ='$id' AND status = 1")->fetch();
-			$user = array('placa' => $row1['placa'],
-				 'cnpj_empresa' => $row1['cnpj_empresa'],
-				 'marca' => $row1['marca'],
-				 'modelo' => $row1['modelo'],
-				 'pneususo' => $row3['pneususo'],
-				 'pneus' => $row2['pneus'],
-				 'disabled' => "disabled",
+			$user = array('cod' => $row1['cod'],
+				 'veiculo' => $row1['veiculo'],
+				 'satus' => $row1['satus'],
+				 'pos' => $row1['pos_veic'],
+				 // 'pneususo' => $row3['pneususo'],
+				 // 'pneus' => $row2['pneus'],
+				 // 'disabled' => "disabled",
 			);
 		}elseif ($_GET['info'] == "c") {
 			$row1 = $dbh->query("SELECT placa,cnpj_empresa,marca,modelo,pneususo,pneus FROM veiculo WHERE placa = '$id' LIMIT 1")->fetch();
-			$user = array('placa' => $row1['placa'],
-				 'cnpj_empresa' => $row1['cnpj_empresa'],
-				 'marca' => $row1['marca'],
-				 'modelo' => $row1['modelo'],
-				 'pneususo' => $row3['pneususo'],
-				 'pneus' => $row2['pneus'],
+			$user = array('cod' => $row1['cod'],
+				 'veiculo' => $row1['veiculo'],
+				 'satus' => $row1['satus'],
+				 'pos' => $row1['pos_veic'],
+				 // 'pneususo' => $row3['pneususo'],
+				 // 'pneus' => $row2['pneus'],
 			);
 		}else{
 			$row = $dbh->query("SELECT placa,cnpj_empresa,marca,modelo FROM veiculo WHERE cnpj_empresa = $cnpj_empresa LIMIT 1")->fetch();
-			$user = array('placa' => $placa,
-				 'cnpj_empresa' => $cnpj_empresa,
-				 'marca' => $marca,
-				 'modelo' => $modelo,
-				 'pneususo' => 0,
-				 'pneus' => 0,
-				 'disabled' => "",
+			$user = array('cod' => $cod,
+				 'veiculo' => $veiculo,
+				 'satus' => $status,
+				 'pos' => $pos,
+				 // 'pneususo' => 0,
+				 // 'pneus' => 0,
+				 // 'disabled' => "",
 			 );
 		}
 	# Exibe os registros na tela
