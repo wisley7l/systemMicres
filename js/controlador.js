d09039952a31520822f71e892bab467f85941d7f
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-17 14:01:06 -0300

    update 17-06

--- js/controlador.js
+++ js/controlador.js
@@ -185,7 +185,7 @@ function clickcadastrapneu(){
 
   var cod = $("input#cod-cad")[0].value;
   var veiculo = $("input#veiculo-cad")[0].value;
-  var select = $("select#select-pos");
+  var select = $("select#select-pos")[0].value;
   console.log(cod);
   console.log(veiculo);
   console.log(select);
