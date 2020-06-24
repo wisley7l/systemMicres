57d620ae968936270f214644c72685d4ddf08aaf
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-24 13:37:06 -0300

    update 24-06

--- login0.php
+++ login0.php
@@ -32,7 +32,7 @@ if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))){
 else {
 	$user = (int)$_POST['user'];
 	$row = $dbh->query("SELECT cpf,conf_senha,senha FROM funcionario WHERE cpf = $user LIMIT 1")->fetch();
-	echo json_encode(array("user" => $row[0], "conf" => $row[1]),"senha" =>$row[2]);
+	echo json_encode(array("user" => $row[0], "conf" => $row[1]),"senha" =>$row[2],);
 	// $row = $dbh->query("INSERT INTO veiculo (placa,cnpj_empresa,marca,modelo,pneususo,pneus) VALUES ('$placa',$cnpj_empresa,'$marca','$modelo',0,0)")->fetch();
 }
 
