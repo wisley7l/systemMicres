c09098a1b2c164ccd36eb1294de7cbac85349083
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-24 15:51:48 -0300

    update 24-06

--- login0.php
+++ login0.php
@@ -25,9 +25,10 @@ if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))){
 	exit;
 }
 else {
+	$f = $_GET['function'];
 	$user = (int)$_POST['user'];
 	$row = $dbh->query("SELECT cpf,conf_senha,senha FROM funcionario WHERE cpf = $user LIMIT 1")->fetch();
-	echo json_encode(array("user" => $row[0], "conf" => $row[1],"senha" => $row[2],"func"=> "$_GET['function']"));
+	echo json_encode(array("user" => $row[0], "conf" => $row[1],"senha" => $row[2],"func"=> $f));
 	// $row = $dbh->query("INSERT INTO veiculo (placa,cnpj_empresa,marca,modelo,pneususo,pneus) VALUES ('$placa',$cnpj_empresa,'$marca','$modelo',0,0)")->fetch();
 
 }
