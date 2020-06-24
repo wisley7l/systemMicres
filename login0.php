990f11c5233cf2be215567938d7b928185dd38a5
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-24 17:05:23 -0300

    update 24-06

--- login0.php
+++ login0.php
@@ -1,23 +1,5 @@
 <?php
-function createSession($id,$email,$name,$tipo)
-{
-	$id_session = $id;
-  session_id($id_session);
-  session_start();
-  // echo $credits;
-  $_SESSION['user_id'] = $id;
-  $_SESSION['user_name'] = $email;
-  $_SESSION['name'] = $name;
-  $_SESSION['login'] = true;
-  $_SESSION['tipo'] = $tipo;
-  if (!is_writable(session_save_path())) {
-  //echo 'Session path "'.session_save_path().'" is not writable for PHP!';
-  }
-  else {
-    header("Location: listaempresa.php");
-    exit();
-  }
-}
+
 
 function login0()
 {
