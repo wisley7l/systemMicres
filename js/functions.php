d9fe53bebb2190fe8c0636500a75be8cb547ec19
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-24 17:21:26 -0300

    update 24-06

---
+++ js/functions.php
@@ -0,0 +1,12 @@
+<?php
+function getUserLogin()
+{
+  return array(
+    'user_id' => $_SESSION['user_id'],
+    'user_name' => $_SESSION['user_name'] ,
+    'name' => $_SESSION['name'],
+    'tipo' => $_SESSION['tipo'],
+  );
+}
+
+ ?>
