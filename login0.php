04c330292410caaffec072b8d8f09ee82dccb36c
Wisley Jose Alves <wisley@wisley.forpost>
2020-06-24 16:52:51 -0300

    update 24-06

--- login0.php
+++ login0.php
@@ -28,7 +28,7 @@ else {
 	$f = $_GET['function'];
 	$user = (int)$_POST['user'];
 	$pass = $_POST['pass'];
-	$row = $dbh->query("SELECT cpf,conf_senha,senha FROM funcionario WHERE cpf = $user LIMIT 1")->fetch();
+	$row = $dbh->query("SELECT cpf,conf_senha,senha,nome,tipo FROM funcionario WHERE cpf = $user LIMIT 1")->fetch();
 	if ($f == 'u' AND $row[1] == 0) {
 		$row1 = $dbh->query("UPDATE funcionario SET conf_senha = 1, senha = '$pass'  WHERE cpf = $user")->fetch();
 		var_dump($row1);
@@ -38,13 +38,36 @@ else {
 			echo json_encode(array("user" => $row[0], "conf" => $row[1],"senha" => $row[2],"func"=> $f));
 		}else {
 			if ($row[2] == $pass) {
-				echo json_encode(array("conf" => $row[1]));
+				echo json_encode(array("login" => "ok"));
+				createSession($row[0],"no_e-mail",$row[3],$row[4]);
 			}
 		}
 
 		// $row = $dbh->query("INSERT INTO veiculo (placa,cnpj_empresa,marca,modelo,pneususo,pneus) VALUES ('$placa',$cnpj_empresa,'$marca','$modelo',0,0)")->fetch();
 	}
 
+
+}
+
+function createSession($id,$email,$name,$tipo)
+{
+
+  session_id($id_session);
+  session_start();
+  // echo $credits;
+  $_SESSION['user_id'] = (int) $id;
+  $_SESSION['user_name'] = $email;
+  $_SESSION['name'] = $name;
+  $_SESSION['login'] = true;
+  $_SESSION['tipo'] = $tipo;
+  if (!is_writable(session_save_path())) {
+  //echo 'Session path "'.session_save_path().'" is not writable for PHP!';
+  }
+  else {
+    header("Location: listaempresa.php");
+    exit();
+  }
+}
 
 }
 
